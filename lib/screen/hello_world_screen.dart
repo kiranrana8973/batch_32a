c0d1d217128b180b07b93d257b68b21d68ef59c3
Kiran Rana <kiranrana8973@gmail.com>
2024-04-16 13:03:40 +0545

    Added label

--- lib/screen/hello_world_screen.dart
+++ lib/screen/hello_world_screen.dart
@@ -5,9 +5,55 @@ class HelloWorldScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Text('Hello worasasdASDAdldddd'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Add two nos',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            // Enter first no
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter first no',
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter Second no',
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Add'),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const Text(
+              'Sum is : 0',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
